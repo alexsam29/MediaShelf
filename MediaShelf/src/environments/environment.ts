09a1e0e8bc9fd52b73dac9ab8255de982daca2fc
Alexander Samaniego <alex_samaniego96@hotmail.com>
2025-08-01 17:15:20 -0400

    Initialize Firebase

---
+++ MediaShelf/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyDxPsmm8L7UT7jitwiwF1FyjN53YXcKil4",
+        authDomain: "mediashelf-2a434.firebaseapp.com",
+        projectId: "mediashelf-2a434",
+        storageBucket: "mediashelf-2a434.firebasestorage.app",
+        messagingSenderId: "550748754464",
+        appId: "1:550748754464:web:1623bd711c770d60ed2245",
+        measurementId: "G-X5FQKWMWV4"
+    }
+};

a322d477df2d9b2c0579fa782a1028084410a502
Alexander Samaniego <alex_samaniego96@hotmail.com>
2025-08-02 00:01:10 -0400

    Implement login and register components; add home component

--- MediaShelf/src/app/app.routes.ts
+++ MediaShelf/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Define your application routes here
+    {
+        path: '',
+        component: HomeComponent,
+        title: "Home Page"
+    }
+];
